d33c2cb1b6ef1fb67eea07beb1f24c500414394a
Chris Rasmussen <chris@digitalformula.net>
2016-07-22 17:57:05 +1000

    Add slugify and shorten functions

--- src/DigitalFormula/Formatting/Text.php
+++ src/DigitalFormula/Formatting/Text.php
@@ -6,7 +6,6 @@
  * @author Chris Rasmussen, digitalformula.net
  * @category Development
  * @license MIT
- * @package DigitalFormula\Formatting
  *
  */
 
@@ -15,7 +14,7 @@ namespace DigitalFormula\Formatting;
 /**
  *
  * The Text class
- *
+ * 
  * @package DigitalFormula\Formatting
  *
  */
@@ -41,4 +40,43 @@ class Text
 		return number_format( $size ) . ' bytes';
 	}
 
+	/**
+	 * @param $string
+	 * @param null $max
+	 * @return string
+	 */
+	public static function shorten( $string, $max = NULL )
+	{
+		if( strlen( $string ) > $max )
+		{
+			return( substr( $string, 0, $max ) . ' ...' );
+		}
+		else
+		{
+			return( $string );
+		}
+	}
+
+	/**
+	 * @param $text
+	 * @return bool|mixed|string
+	 */
+	public static function slugify( $text )
+	{
+		/* replace non letter or digits by - */
+		$text = preg_replace( '~[^\\pL\d]+~u', '-', $text );
+		/* trim */
+		$text = trim( $text, '-' );
+		/* transliterate */
+		$text = iconv( 'utf-8', 'us-ascii//TRANSLIT', $text );
+		/* lowercase */
+		$text = strtolower( $text );
+		/* remove unwanted characters */
+		$text = preg_replace( '~[^-\w]+~', '', $text );
+		if ( empty( $text ) ) {
+			return 'n-a';
+		}
+		return $text;
+	}
+	
 }
